3291319417feb41c0afcd11e225fe8e5ede24ec0
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2023-09-22 12:22:49 +0300

    updated

---
+++ elev8_Advanced/Projects/expense_tracker_react_app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <header className="App-header">
+          <h1>Welcome to React</h1>
+        </header>
+      </div>
+  );
+}
+
+export default App;
